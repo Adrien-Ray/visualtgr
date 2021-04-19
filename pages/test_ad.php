1856f42a5a3e99fcd913344c90823d0a80860183
Adrien-Ray <forpub@accesdenied.net>
2021-04-19 16:14:41 +0200

    demarage page test elements

---
+++ pages/test_ad.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="../assets/images/logo.png" type="image/x-icon">
+    <title>Visualtgr</title>
+    <link rel="stylesheet" href="../assets/styles/main.css">
+</head>
+<body>
+    <div class="backdark">
+    <!-- BOUTTONS NAVBAR -->
+    <a class="nav-link button1" href="#">Features</a>
+    <!-- FIN BUTTONS NAVBAR 
+    la class nav-link fera reference au code boostrap
+    -->
+    </div>
+</body>
+</html>
