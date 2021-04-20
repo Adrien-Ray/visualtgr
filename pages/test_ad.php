4613a79879a51071ccd2592a21038a21b6579c6f
Adrien-Ray <forpub@accesdenied.net>
2021-04-20 11:52:40 +0200

    plusieurs-css

--- pages/test_ad.php
+++ pages/test_ad.php
@@ -6,7 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="../assets/images/logo.png" type="image/x-icon">
     <title>Visualtgr</title>
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous"> -->
     <link rel="stylesheet" href="../assets/styles/main.css">
+    <link rel="stylesheet" href="../assets/styles/test.css">
 </head>
 <body>
     <div class="backdark">
@@ -16,5 +18,9 @@
     la class nav-link fera reference au code boostrap
     -->
     </div>
+    <div class="backdark">
+    <!-- DEBUT BLOC-ELEMENT NAVBAR -->
+    </div>
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script> -->
 </body>
 </html>
