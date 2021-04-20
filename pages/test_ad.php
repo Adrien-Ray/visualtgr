cadf8a753a67fd26ff60cc5b9fe01bc2c8db9a0e
Adrien-Ray <forpub@accesdenied.net>
2021-04-20 13:53:18 +0200

    pret au merge

--- pages/test_ad.php
+++ pages/test_ad.php
@@ -6,21 +6,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="../assets/images/logo.png" type="image/x-icon">
     <title>Visualtgr</title>
-    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous"> -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <link rel="stylesheet" href="../assets/styles/main.css">
     <link rel="stylesheet" href="../assets/styles/test.css">
 </head>
 <body>
     <div class="backdark">
     <!-- BOUTTONS NAVBAR -->
-    <a class="nav-link button1" href="#">Features</a>
+    <a class="nav-link button1" href="#">Features</a><span class="backdarkspan">boutons de navbar</span>
     <!-- FIN BUTTONS NAVBAR 
     la class nav-link fera reference au code boostrap
     -->
     </div>
-    <div class="backdark">
-    <!-- DEBUT BLOC-ELEMENT NAVBAR -->
+    <div class="backlight">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 col-sm-1">
+    <!-- DEBUT BLOC-ELEMENT NAVBAR  -->
+    <div class="card card-left">
+        <span class="backdarkspan">card-left</span>
     </div>
-    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script> -->
+    <!-- FIN BLOC-ELEMENT NAVBAR -->
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
 </body>
 </html>
