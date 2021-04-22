1ebe6bad719110fd9dfd187449ea33c9d90c9066
etiennezastko <etiennezastko@hotmail.fr>
2021-04-22 13:47:53 +0200

    new version

--- pages/index.php
+++ pages/index.php
@@ -1,3 +1,145 @@
 <?php include "../includes/header.php"; ?>
 
+<div class="container">
+    <div class="row">
+        <!--  Première section -->
+        <div class="card mb-3 card-body-child" style="max-width: 1296px;">
+            <div class="row g-0">
+                <div class="col-md-6">
+                    <img src="../assets/images/image-8.png" alt="image-8.png">
+                </div>
+                <div class="col-md-6">
+                    <div class="card-body">
+                        <h5 class="card-title card_title--modificateur">Le métier de de developeur web</h5>
+                        <p class="card-text card_text--modificateur">Qu’est-ce qu’un développeur web ?
+                            Le développeur rédige le code informatique d’un produit / site / programme répondant aux exigences du cahier des charges rédigé par le chef de projet en fonction des exigences du client et des besoins fonctionnels.
+                            Il analyse les besoins, choisit la solution technique la mieux adaptée et développe les fonctionnalités du site web ou de l’application web de sa phase d’étude à son intégration.
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Deuxime section -->
+    <div class="container">
+        <div class="row">
+            <div class="card mb-3 " style="max-width: 1296px;">
+                <div class="row g-0 ">
+                    <div class="col-md-5 ">
+                        <div class="card-body ">
+
+
+                            <h5 class="card-title card_title--modificateur">Se déplacer dans le terminal. Les bases.
+                                </h2>
+
+                                <p class="card-text card_text--modificateur ">Dans un premier temps nous allons voir ensemble la commande «ls ». Elle sert à explorer le contenue du dossier dans lequel vous vous trouvez. La commande ls sert aussi à explorer un dossier définit. ls /nom du dossier. Découvrons maintenant…</p>
+
+                                <button class="btn-btn-primary section_button-1" type="submit">flex
+
+                                </button>
+                        </div>
+                    </div>
+                    <div class="col-md-7 ">
+
+                        <img src="../assets/images/img-2.jpg" alt="img-2.jpg">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- troième section -->
+    <div class="container">
+        <div class="row">
+            <div class="card mb-3 " style="max-width: 1296px;">
+                <div class="row g-0 ">
+                    <div class="col-md-5 ">
+                        <div class="card-body ">
+
+
+                            <h5 class="card-title card_title--modificateur">Les Tutos rigolo
+                                </h2>
+
+                                <p class="card-text card_text--modificateur ">Tuto rigolo # 1 animé un bouton sur Figma en 2min40
+
+                                    Pourquoi les animations sur Figma sont essentiels ? D’une part cela permet de rendre votre maquette plus réaliste, et d’autre part, de vous rendre compte de ce qui va et de ce qui ne va pas.
+
+                                    Ici rien de bien sorcier, en quelque clics de souri, vous arriverez a faire un truc plutôt sympa et qui vous servira quoi que vous fassiez.
+
+                                    Bon visionnage .</p>
+
+                                <button class="btn-btn-primary section_button-1" type="submit">flex
+
+                                </button>
+                        </div>
+                    </div>
+                    <div class="col-md-7 ">
+
+                        <img src="../assets/images/image-3.jpg" alt="image-3.jpg">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Quatrième section -->
+    <div class="card-group">
+        <div class="card">
+            <img src="../assets/images/flex-box.png" class=" card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title card_title--modificateur">CSS FLEXBOX</h5>
+                <p class="card-text card_text--modificateur ">Les flexbox qu’est ce que c’est ? Le module des boîtes flexibles, aussi appelé « flexbox », a été conçu comme un modèle de disposition unidimensionnel et comme une méthode permettant de distribuer l’espace entre des objets d’une interface ainsi que de…</p>
+
+            </div>
+        </div>
+        <!-- cinquième section -->
+        <div class="card">
+            <img src="../assets/images/css.png" class="card-img-top" alt="css.png">
+            <div class="card-body">
+                <h5 class="card-title card_title--modificateur">Le CSS</h5>
+                <p class="card-text card-text card_text--modificateur">Une fois notre page HTML rédigée, nous avons besoin de lui donner forme avec des styles CSS propres à chaque élément. Voyons comment « parler » au CSS pour lui expliquer quel élément HTML il doit modifier et dans quelles circonstances. /*…</p>
+
+            </div>
+        </div>
+        <!-- sixième section -->
+        <div class="card">
+            <img src="../assets/images/media-queries.jpg" class="card-img-top" alt="media-queries.jpg">
+            <div class="card-body">
+                <h5 class="card-title card_title--modificateur">Le Media-queries</h5>
+                <p class="card-text card_text--modificateur">Les requêtes média (media queries) permettent de modifier l’apparence d’un site ou d’une application en fonction du type d’appareil (impression ou écran par exemple) et de ses caractéristiques (la résolution d’écran ou la largeur de la zone d’affichage (viewport) par…</p>
+            </div>
+        </div>
+    </div>
+    <!-- Septième section -->
+    <div class="card mb-3" style="max-width: 1296px;">
+        <div class="row g-0">
+            <div class="col-md-6">
+                <img src="../assets/images/matrix.png" alt="matrix.png">
+            </div>
+            <div class="col-md-6">
+                <div class="card-body">
+                    <h5 class="card-title card_title--modificateur">la Cryptographie ou l’art du secret.</h5>
+                    <p class="card-text card_text--modificateur">La cryptographie regroupe les disciplines ayant pour but de rechercher des moyens d’assurer la protection de données. cela inclu: la confidentialitée: empêcher la lecture d’un message par une personne non-autorisée. l’authenticitée: permettre de « signer » un message pour assurer qu’il est…
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+
+<!-- <div class="card mb-3 guideline" style="max-width: 1000px;">
+    <div class="row g-0 guideline">
+        <div class="col-md-6 guideline">
+            <div class="card-body guideline">
+                <p class="subtitle guideline">INGENIEUR DIETER WEISS </p>
+
+                <h2 class="card-title guideline">
+                    Ingenieurbûro fûr Bau- und Vermessungswesen</h2> -->
 <?php include "../includes/footer.php"; ?>
