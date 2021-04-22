e3e6fb9bba4901917fb89fa587b436c26d3b6135
Adrien-Ray <forpub@accesdenied.net>
2021-04-22 20:47:47 +0200

    ad push force!

--- pages/art1.php
+++ pages/art1.php
@@ -4,78 +4,78 @@
         <div class="col-md-12 col-sm-12 content">
             <h1>Le métier de développeur web</h1>
             <i>chloev 2 avril 2021- catégorie Les métiers du web</i>
-            <img src="../assets/images/art1.jpg" alt="">
+            <br>
+            <p class="img"><img src="../assets/images/art1.jpg" alt=""></p>
             <h2>Qu’est-ce qu’un développeur web ?</h2>
-            <p>Le développeur rédige le code informatique d’un produit / site / programme répondant aux exigences du cahier des charges rédigé par le chef de projet en fonction des exigences du client et des besoins fonctionnels.Il analyse les besoins, choisit la solution technique la mieux adaptée et développe les fonctionnalités du site web ou de l’application web de sa phase d’étude à son intégration.</p>
+            <div class="content__darkBack">
+                <p>Le développeur rédige le code informatique d’un produit / site / programme répondant aux exigences du cahier des charges rédigé par le chef de projet en fonction des exigences du client et des besoins fonctionnels.Il analyse les besoins, choisit la solution technique la mieux adaptée et développe les fonctionnalités du site web ou de l’application web de sa phase d’étude à son intégration.</p>
+            </div>
             <h2>Autres missions :</h2>
-            <ul>
-                <li>corriger les problèmes des projets développés sans interrompre le fonctionnement / assurer le support technique</li>
-                <li>former le client à la réception du site</li>
-                <li>rédiger notices et guides pour les utilisateurs</li>
-            </ul>
+            <div class="content__darkBack">
+                <ul>
+                    <li>corriger les problèmes des projets développés sans interrompre le fonctionnement / assurer le support technique</li>
+                    <li>former le client à la réception du site</li>
+                    <li>rédiger notices et guides pour les utilisateurs</li>
+                </ul>
+            </div>
             <h2>Les catégories de développeur web</h2>
-            <h3>Développeur back-end :</h3>
-            <p>Il s’occupe de la programmation du côté du serveur : il crée le code et les programmes qui alimentent le serveur du site web, les bases de données et toutes les applications que contient le site internet. Il utilise les différents langages côté serveur :</p>
-            <ul>
-                <li>PHP</li>
-                <li>Python</li>
-                <li>Java</li>
-                <li>Ruby</li>
-                <li>SQL (pour gérer les données dans les bases de données des sites)</li>
-            </ul>
-            <h3>Développeur front-end :</h3>
-            <p>Il se charge de la conception du site web côté client et écrit le code nécessaire pour l’implémenter sur le web. Il utilise les différents langages côté client :</p>
-            <ul>
-                <li>HTML</li>
-                <li>CSS</li>
-                <li>JavaScript</li>
-            </ul>
-            <p>Il peut également réaliser :</p>
-            <ul>
-                <li>le référencement naturel (SEO)</li>
-                <li>les tests d’utilisabilité, d’accessibilité et de performances</li>
-            </ul>
-            <h3>Développeur full stack</h3>
-            <p>Il réunit les 2 facettes front-end et back-end.</p>
+            <div class="content__darkBack">
+                <h3>Développeur back-end :</h3>
+                <p>Il s’occupe de la programmation du côté du serveur : il crée le code et les programmes qui alimentent le serveur du site web, les bases de données et toutes les applications que contient le site internet. Il utilise les différents langages côté serveur :</p>
+                <ul>
+                    <li>PHP</li>
+                    <li>Python</li>
+                    <li>Java</li>
+                    <li>Ruby</li>
+                    <li>SQL (pour gérer les données dans les bases de données des sites)</li>
+                </ul>
+                <h3>Développeur front-end :</h3>
+                <p>Il se charge de la conception du site web côté client et écrit le code nécessaire pour l’implémenter sur le web. Il utilise les différents langages côté client :</p>
+                <ul>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JavaScript</li>
+                </ul>
+                <p>Il peut également réaliser :</p>
+                <ul>
+                    <li>le référencement naturel (SEO)</li>
+                    <li>les tests d’utilisabilité, d’accessibilité et de performances</li>
+                </ul>
+                <h3>Développeur full stack</h3>
+                <p>Il réunit les 2 facettes front-end et back-end.</p>
+                <h3>Développeur web mobile</h3>
+                <p>Il effectue la réalisation technique et le développement informatique d’applications pour mobiles, smartphones et autres tablettes. Ses responsabilités sont de tenir compte des spécificités de l’affichage des applications sur les différents outils. Pour cela, il utilise différents langages selon les systèmes d’exploitation :</p>
+                <ul>
+                    <li>C / Objective C / HTML5 (iOS)</li>
+                    <li>C / C# / PHP / Java (Android)</li>
+                </ul>
+                <p>Deux possibilités s’offrent à lui :</p>
+                <ul>
+                    <li>Adapter un site internet classique pour une application mobile</li>
+                    <li>Créer une application mobile qui n’a pas de site internet existant.</li>
+                </ul>
+            </div>
+            <h2>Qualités recherchées</h2>
+            <div class="content__darkBack">
+                <ul>
+                    <li>polyvalence</li>
+                    <li>autonomie</li>
+                    <li>respect des délais</li>
+                    <li>veille technologique</li>
+                </ul>
+                <h2>Où exerce t-on ces métiers ?</h2>
+                <p>Cette activité s’exerce au sein de sociétés de services, d’entreprises, de collectivités territoriales, d’entreprises publiques, en indépendant etc. Elle varie selon la taille de la structure (polyvalence, spécialisation en gestion du projet, programmation, recettes, etc.) et la responsabilité (développement, projet, etc). Le salaire est également fonction de ces différents paramètres. En moyenne, un développeur web junior peut percevoir une rémunération brute mensuelle de 2100 €. Mais certains atteignent les étoiles tels les développeurs de la Silicon Valley qui peuvent recevoir un salaire de 500 000 $ … cela laisse rêveur !</p>
+            </div>
+            <h2>Pour en savoir +</h2>
+            <div class="content__darkBack">
+                <ul>
+                    <li>https://www.opiiec.fr/metiers/83057</li>
+                </ul>
+                <p class="img"><img src="../assets/images/art-01-0.png" alt=""></p>
+            </div>
         </div>
     </div>
 </div>
 
-
-Développeur web mobile
-
-Il effectue la réalisation technique et le développement informatique d’applications pour mobiles, smartphones et autres tablettes.
-
-Ses responsabilités sont de tenir compte des spécificités de l’affichage des applications sur les différents outils.
-
-Pour cela, il utilise différents langages selon les systèmes d’exploitation :
-
-    C / Objective C / HTML5 (iOS)
-    C / C# / PHP / Java (Android)
-
-Deux possibilités s’offrent à lui :
-
-    Adapter un site internet classique pour une application mobile
-    Créer une application mobile qui n’a pas de site internet existant.
-
-Qualités recherchées
-
-    polyvalence
-    autonomie
-    respect des délais
-    veille technologique
-
-Où exerce t-on ces métiers ?
-
-Cette activité s’exerce au sein de sociétés de services, d’entreprises, de collectivités territoriales, d’entreprises publiques, en indépendant etc.
-
-Elle varie selon la taille de la structure (polyvalence, spécialisation en gestion du projet, programmation, recettes, etc.) et la responsabilité (développement, projet, etc).
-
-Le salaire est également fonction de ces différents paramètres. En moyenne, un développeur web junior peut percevoir une rémunération brute mensuelle de 2100 €. Mais certains atteignent les étoiles tels les développeurs de la Silicon Valley qui peuvent recevoir un salaire de 500 000 $ … cela laisse rêveur !
-Pour en savoir +
-
-https://www.opiiec.fr/metiers/83057
-
-<img src="../assets/images/art-01-0.png" alt="">
 
 <?php include "../includes/footer.php"; ?>
