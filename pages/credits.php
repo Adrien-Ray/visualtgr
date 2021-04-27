7e1549ffea2ef914fdcfa506cdf5bdd1ae0c5e1d
Adrien-Ray <forpub@accesdenied.net>
2021-04-27 13:38:19 +0200

    credits2

--- pages/credits.php
+++ pages/credits.php
@@ -10,24 +10,9 @@
             <h2>introduction</h2>
 
             <p>VISUALTGR est un site internet qui consiste à mettre en avant des articles relatif aux principaux metiers du web-design.  Le code source du site internet est <a target="_blank" href="https://github.com/Fire-Owl/adrien-etienne/tree/main">accecible à tous</a>. IL peut être modifié par toute les personne ayant des connaissance dans ce domaine, afin de pouvoir rendre ce site un espace de partage et ansi d’apporter des mise à jour de plus en plus en fonctionnel et créative à notre projet, le code est bien évidement disponible sur notre GIThup : VISUALTGR </p>
-            <p>Les conditions d'utilisation et les règles relatives à la copie sont clairement répertoriées dans la licence publique générale GNU . Toutes les clauses de GNU sont disponible ici :www.gnu.org</p>
+            <p>Les conditions d'utilisation et les règles relatives à la copie sont clairement répertoriées dans la licence publique générale GNU . Toutes les clauses de GNU sont disponible ici : <a href="https://www.gnu.org" target="_blank" rel="noopener noreferrer">www.gnu.org</a>.</p>
             <p class="img"><img src="../assets/images/license-1.png" class="logo" alt="license-1.png"></p>
         </div>
     </div>
 </div>
-
-
-
-Ce site est sous license de gni (gpl free software v3)
-<!-- Ce site est sous license de gni (gpl free software v3) :
-
-introduction
-VISUALTGR est un site internet qui consiste à mettre en avant des articles relatif aux principaux metiers du web-design. 
-Le code source du site internet est accecible à tous. IL peut être modifié par toute les personne ayant des connaissance dans ce domaine, afin de pouvoir rendre ce site un espace de partage et ansi d’apporter des mise à jour de plus en plus  en fonctionnel et créative à notre projet, le code est bien évidement 
-disponible sur notre GIThup : VISUALTGR 
-  
-Les conditions d'utilisation et les règles relatives à la copie sont clairement répertoriées dans la licence publique générale GNU .
-Toutes les clauses de GNU sont disponible ici :www.gnu.org
-
- -->
 <?php include "../includes/footer.php"; ?>
