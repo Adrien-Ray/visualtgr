58507b2ab106a8b09fbf342d9d4dca7923d1f7c9
Adrien-Ray <forpub@accesdenied.net>
2021-04-27 13:34:54 +0200

    credits

--- pages/credits.php
+++ pages/credits.php
@@ -9,15 +9,9 @@
 
             <h2>introduction</h2>
 
-            <p>VISUALTGR est un site internet qui consiste à mettre en avant des articles relatif aux principaux metiers du web-design.
-                Le code source du site internet est accecible à tous. IL peut être modifié par toute les personne ayant des connaissance dans ce domaine, afin de pouvoir rendre ce site un espace de partage et ansi d’apporter des mise à jour de plus en plus en fonctionnel et créative à notre projet, le code est bien évidement
-                disponible sur notre GIThup : VISUALTGR </p>
-
-            <p>Les conditions d'utilisation et les règles relatives à la copie sont clairement répertoriées dans la licence publique générale GNU .
-                Toutes les clauses de GNU sont disponible ici :www.gnu.org</p>
-
-
-            <p class="img"><img src="../assets/images/license-1.png" class="card-img-top" alt="license-1.png"></p>
+            <p>VISUALTGR est un site internet qui consiste à mettre en avant des articles relatif aux principaux metiers du web-design.  Le code source du site internet est <a target="_blank" href="https://github.com/Fire-Owl/adrien-etienne/tree/main">accecible à tous</a>. IL peut être modifié par toute les personne ayant des connaissance dans ce domaine, afin de pouvoir rendre ce site un espace de partage et ansi d’apporter des mise à jour de plus en plus en fonctionnel et créative à notre projet, le code est bien évidement disponible sur notre GIThup : VISUALTGR </p>
+            <p>Les conditions d'utilisation et les règles relatives à la copie sont clairement répertoriées dans la licence publique générale GNU . Toutes les clauses de GNU sont disponible ici :www.gnu.org</p>
+            <p class="img"><img src="../assets/images/license-1.png" class="logo" alt="license-1.png"></p>
         </div>
     </div>
 </div>
