f2c74faac657f8c3795a0cc8700085edbc90880e
Adrien-Ray <forpub@accesdenied.net>
2021-05-03 13:10:13 +0200

    credits

--- pages/credits.php
+++ pages/credits.php
@@ -4,12 +4,12 @@
         <div class="col-md-12 col-sm-12 content">
 
             <i>Adrien Raymond & Etienne Zastko</i>
-            <h1>Ce site est sous license de gni (gpl free software v3):</h1>
+            <h1>Ce site est sous license de GNU (gpl free software v3):</h1>
 
 
             <h2>introduction</h2>
 
-            <p>VISUALTGR est un site internet qui consiste à mettre en avant des articles relatif aux principaux metiers du web-design.  Le code source du site internet est <a target="_blank" href="https://github.com/Fire-Owl/adrien-etienne/tree/main">accecible à tous</a>. IL peut être modifié par toute les personne ayant des connaissance dans ce domaine, afin de pouvoir rendre ce site un espace de partage et ansi d’apporter des mise à jour de plus en plus en fonctionnel et créative à notre projet, le code est bien évidement disponible sur notre GIThup : VISUALTGR </p>
+            <p>VISUALTGR est un site internet qui consiste à mettre en avant des articles relatif aux principaux metiers du web-design. Le code source du site internet est <a target="_blank" href="https://github.com/Fire-Owl/adrien-etienne/tree/main">accecible à tous</a>. IL peut être modifié selon la licence, le code est bien évidement <a target="_blank" href="https://github.com/Fire-Owl/adrien-etienne/tree/main">disponible sur notre GitHub : VISUALTGR</a></p>
             <p>Les conditions d'utilisation et les règles relatives à la copie sont clairement répertoriées dans la licence publique générale GNU . Toutes les clauses de GNU sont disponible ici : <a href="https://www.gnu.org" target="_blank" rel="noopener noreferrer">www.gnu.org</a>.</p>
             <p class="img"><img src="../assets/images/license-1.png" class="logo" alt="license-1.png"></p>
             <p>Ce site relaie des articles issus de <a href="https://promo-68.codeur.online/blog/" target="_blank" rel="noopener noreferrer">promo-68.codeur.online/blog/</a></p>
