9d99bc41245f5ac607ac6ec8110680c9c4b9db29
Adrien-Ray <forpub@accesdenied.net>
2021-05-04 16:49:40 +0200

    finit??

--- pages/credits.php
+++ pages/credits.php
@@ -6,13 +6,12 @@
             <i>Adrien Raymond & Etienne Zastko</i>
             <h1>Ce site est sous license de GNU (gpl free software v3):</h1>
 
-
-            <h2>introduction</h2>
-
             <p>VISUALTGR est un site internet qui consiste à mettre en avant des articles relatif aux principaux metiers du web-design. Le code source du site internet est <a target="_blank" href="https://github.com/Fire-Owl/adrien-etienne/tree/main">accecible à tous</a>. IL peut être modifié selon la licence, le code est bien évidement <a target="_blank" href="https://github.com/Fire-Owl/adrien-etienne/tree/main">disponible sur notre GitHub : VISUALTGR</a></p>
             <p>Les conditions d'utilisation et les règles relatives à la copie sont clairement répertoriées dans la licence publique générale GNU . Toutes les clauses de GNU sont disponible ici : <a href="https://www.gnu.org" target="_blank" rel="noopener noreferrer">www.gnu.org</a>.</p>
             <p class="img"><img src="../assets/images/license-1.png" class="logo" alt="license-1.png"></p>
             <p>Ce site relaie des articles issus de <a href="https://promo-68.codeur.online/blog/" target="_blank" rel="noopener noreferrer">promo-68.codeur.online/blog/</a></p>
+            <p>Ce site utilise la police d'écriture PixelOperator, sous licence <a href="../assets/styles/LICENSE.txt">Creative Commons</a></p>
+            <p>Site héberger par <a href="https://www.onlineformapro.com/">onlineformapro</a>.</p>
         </div>
     </div>
 </div>
