d2c037367c7d4fe9ad128f7cbb1a4a55f4f5123e
Adrien-Ray <forpub@accesdenied.net>
2021-05-04 14:50:09 +0200

    meta

--- includes/header.php
+++ includes/header.php
@@ -5,6 +5,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="Visualtgr - blog de vulgarisation de l'informatique professionnel, a destination des pros et des autodidactes">
   <link rel="shortcut icon" href="../assets/images/logo.png" type="image/x-icon">
   <title>Visualtgr</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
