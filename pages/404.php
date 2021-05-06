501107b3e737d3894224b488cd39426c6bfe1463
Adrien-Ray <forpub@accesdenied.net>
2021-05-06 10:49:51 +0200

    htaccess

---
+++ pages/404.php
@@ -0,0 +1,3 @@
+<?php include "https://adrienr.promo-68.codeur.online/visualtgr/includes/header.php"; ?>
+<h1>oups, cette page n'existe pas...</h1>
+<?php include "https://adrienr.promo-68.codeur.online/visualtgr/includes/footer.php"; ?>
